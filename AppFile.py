16ddf7ee471676c85ff8b3a637a5f0914474bf78
SamMatsiko <13479800+SamMatsiko@users.noreply.github.com>
2024-07-13 18:05:49 +0300

    Update AppFile.py

--- AppFile.py
+++ AppFile.py
@@ -60,7 +60,7 @@ if keywords_input:
         
         df = pd.DataFrame(articles_data)
         df['Published At'] = pd.to_datetime(df['Published At'])
-        df['Date'] = df['Published At'].dt.date        
+        df['Publication Date'] = df['Published At'].dt.date        
         df=df[df['Source']!='[Removed]']                  
         df.loc[:,'Sentiment'] = [afn.score(str(article)) for article in df.loc[:,'Title']]       
 
@@ -73,7 +73,7 @@ if keywords_input:
         
 
         # Convert URLs to clickable links in the DataFrame
-        df['URL'] = df['URL'].apply(lambda x: x.replace('news link', '<b>news link</b>'))
+        df['News Link'] = df['URL'].apply(lambda x: x.replace('news link', '<b>news link</b>'))
         df=df[['Source','Date','Title','URL','Sentiment']]
 
         # Pagination variables
